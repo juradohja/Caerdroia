92bcb019e6b486715a93f36bcef6bec4745c20db
ErickQuintanar <erick.quintanar@hotmail.com>
2016-10-28 19:09:42 -0500

    Modified: Mapa

--- Mapa.java
+++ Mapa.java
@@ -2,6 +2,8 @@ public class Mapa{
 	
 	private Personaje heroe;
 	private Camara[5][5] camaras;
+	private int posicionX=0;
+	private int posicionY=0;
 
 	public Mapa(Personaje heroe){
 		this.heroe = heroe;
@@ -21,5 +23,21 @@ public class Mapa{
 
 	public Camara getCamara(int i, int j){
 		return camaras[i][j];
+	}
+
+	public void setPosicionX(int posicionX){
+		posicionX = this.posicionX;
+	}
+
+	public int getPosicionX(){
+		return posicionX;
+	}
+
+	public void setPosicionY(int posicionY){
+		posicionY = this.posicionY;
+	}
+
+	public int getPosicionY(){
+		return posicionY;
 	}
 }
