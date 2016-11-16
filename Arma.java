319258fa0551ff2042f10f66401a653074df685e
Mau <reverendo_siegfried@live.com.mx>
2016-11-15 23:26:34 -0600

    Añadí las armas y sus nombres con stats

--- Arma.java
+++ Arma.java
@@ -26,11 +26,64 @@ public class Arma extends Equipo{
 	private void setAttributes(int tipo){
 		switch(tipo){
 			case 0:
-			nombre = "Martillo";
-			fuerza = 50;
+			nombre = "Espada Gigante";
+			fuerza =70;
 			break;
 
-			//completar
+			case 1:
+			nombre = "Mazo";
+			fuerza= 20;
+			break;
+
+			case 2:
+			nombre = "Hacha Pesada";
+			fuerza= 35;
+			break;
+
+			case 3:
+			nombre = "Cuchilla Maldita";
+			fuerza= 100;
+			break;
+
+			case 4:
+			nombre = "Tenedor";
+			fuerza= 5;
+			break;
+
+			case 5:
+			nombre = "Katana";
+			fuerza= 50;
+			break;
+
+			case 6:
+			nombre = "Tridente de Hielo";
+			magia= 35;
+			break;
+
+			case 7:
+			nombre = "Cetro Oscuro";
+			magia= 70;
+			break;
+
+			case 8:
+			nombre = "Orbe";
+			magia= 50;
+			break;
+
+			case 9:
+			nombre = "Palito";
+			magia= 5;
+			break;
+
+			case 10:
+			nombre = "Báculo";
+			magia= 20;
+			break;
+
+			case 11:
+			nombre = "Motosierra";
+			magia= 100;
+			break;
 		}
 	}
 }
