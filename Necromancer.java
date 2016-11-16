8af78b82e98cb9e4061122da72b76b37451580cb
Mau <reverendo_siegfried@live.com.mx>
2016-11-15 23:29:03 -0600

    aumento de estas del enemigo

--- Necromancer.java
+++ Necromancer.java
@@ -5,5 +5,13 @@ public class Necromancer extends Personaje{
 		super(nombre, vida, fuerza, magia, resistencia, inteligencia, iniciativa,
 		experiencia, nivel, bolsa);
 	}
-
+	
+	public Necromancer(int nivel){
+		vida = 75*nivel;
+		fuerza = 7*nivel;
+		magia = 16*nivel;
+		resistencia = 3*nivel;
+		inteligencia = 4*nivel;
+		iniciativa = 8*nivel;
+	}
 }
