e15b6d3fcf7ecb4180d04ac129224daa31c3eca6
Mau <reverendo_siegfried@live.com.mx>
2016-11-22 03:14:25 -0600

    Update Necromancer.java

--- Necromancer.java
+++ Necromancer.java
@@ -8,7 +8,6 @@ public class Necromancer extends Personaje{
 		magia = magia*16;
 		resistencia = resistencia*3;
 		inteligencia = inteligencia*4;
-		iniciativa = iniciativa*8;
 		hFisica = new Habilidad("Necrosis", 35, 15);
 		hMagica = new Habilidad("Toque Cadavérico", 10, 80);
 	}
@@ -21,7 +20,6 @@ public class Necromancer extends Personaje{
 		magia = magia*16;
 		resistencia = resistencia*3;
 		inteligencia = inteligencia*4;
-		iniciativa = iniciativa*8;
 		hFisica = new Habilidad("Necrosis", 35, 15);
 		hMagica = new Habilidad("Toque Cadavérico", 10, 80);
 	}
