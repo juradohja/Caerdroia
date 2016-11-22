8370cddceccfa9f88b6a659b59910c2ff19b9745
Mau <reverendo_siegfried@live.com.mx>
2016-11-22 00:47:01 -0600

    Update Accesorio.java
    añado los accesorios

--- Accesorio.java
+++ Accesorio.java
@@ -9,14 +9,66 @@ public class Accesorio extends Equipo{
 	public Accesorio(boolean starter){
 		super(starter);
 		if(starter){
-			setAttributes(0);
+			setAttributes(12);
 		} else {
-			setAttributes(random.nextInt()); // número de armas que hay
+			setAttributes(random.nextInt(12)); // número de armas que hay
 		}
 	}
 
 	protected void setAttributes(int tipo){
-		
+		switch(tipo){
+			case 0: // CAMBIAR CASO CERO A STARTER
+			nombre = "Anillo del Rey";
+			break;
+
+			case 1:
+			nombre = "Anillo de Boda";
+			break;
+
+			case 2:
+			nombre = "Collar de Gemas";
+			break;
+
+			case 3:
+			nombre = "Moneda";
+			break;
+
+			case 4:
+			nombre = "Alma Perdida";
+			break;
+
+			case 5:
+			nombre = "Pulsera de Goma";
+			break;
+
+			case 6:
+			nombre = "Hada en Botella";
+			break;
+
+			case 7:
+			nombre = "Espejo Maldito";
+			break;
+
+			case 8:
+			nombre = "Foto de Gatito";
+			break;
+
+			case 9:
+			nombre = "Cuerno de Demonio";
+			break;
+
+			case 10:
+			nombre = "Libro de Magia";
+			break;
+
+			case 11:
+			nombre = "Vitamina";
+			break;
+
+			case 12:
+			nombre = "Pelusa";
+			break;
+		}
 	}
 
 }
