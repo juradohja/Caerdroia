72836ea4117e836d591879b554f2cdf20ebaf81a
Mau <reverendo_siegfried@live.com.mx>
2016-11-22 01:17:41 -0600

    Añado a los stats del jefe

--- Jefe.java
+++ Jefe.java
@@ -2,8 +2,15 @@ public class Jefe extends Personaje{
 
 	public Jefe(int nivel){
 		super(nivel);
-		hFisica = new Habilidad("Habilidad Física", 0, 0);
-		hMagica = new Habilidad("Habilidad Mágica", 0, 0);
+		vida = vida*140;
+		maxVida = vida;
+		fuerza = fuerza*11;
+		magia = magia*11;
+		resistencia = resistencia*8;
+		inteligencia = inteligencia*8;
+		iniciativa = iniciativa*8;
+		hFisica = new Habilidad("Omega", 60, 30);
+		hMagica = new Habilidad("Oblivion", 30, 60);
 	}
 
 }
